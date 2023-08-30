f59a875ac585a2c8719200d5fbdbc2ecd816c030
Anna Korbelová <137218395+AnnaKorbelova@users.noreply.github.com>
2023-08-31 00:28:58 +0200

    Update answer_3.sql

--- answer_3.sql
+++ answer_3.sql
@@ -5,5 +5,5 @@ CREATE OR REPLACE VIEW v_anna_korbelova_sql_answer_3 AS
 		AVG(food_average_price) AS price_growth_by_year 
 	FROM v_anna_korbelova_project_sql_primary_final vakpspf 
 	GROUP BY food_category,
-		 	 payroll_year  
+		 payroll_year  
 	ORDER BY price_growth_by_year;
