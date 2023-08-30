59b4d146ce6d4f86c8c90a33878738a83209f15a
Anna Korbelová <137218395+AnnaKorbelova@users.noreply.github.com>
2023-08-30 22:59:54 +0200

    Update answer_1.sql

--- answer_1.sql
+++ answer_1.sql
@@ -6,6 +6,6 @@ CREATE OR REPLACE VIEW v_anna_korbelova_sql_answer_1 AS
 	FROM v_anna_korbelova_project_sql_primary_final vakpspf
 	WHERE industry_name IS NOT NULL
 	GROUP BY payroll_year, 
-			 industry_name 
+		 industry_name 
 	ORDER BY industry_name, 
-			  payroll_year; 
+	         payroll_year; 
