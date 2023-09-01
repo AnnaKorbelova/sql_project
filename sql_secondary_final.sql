f3401c70b578edc0b96100bb892f8bd10186f4c8
Anna Korbelová <137218395+AnnaKorbelova@users.noreply.github.com>
2023-09-01 11:16:53 +0200

    Update sql_secondary_final.sql

--- sql_secondary_final.sql
+++ sql_secondary_final.sql
@@ -1,19 +1,19 @@
 CREATE OR REPLACE VIEW v_anna_korbelova_project_sql_secondary_final AS
 WITH Europe AS (
-SELECT 
+	SELECT 
 		country,
 		GDP,
 		gini,
 		population,
 		year
-FROM economies e 
-WHERE e.country LIKE '%euro%'
-)
-	SELECT 
-	* 	
-	FROM Europe
-	WHERE year >= '2006'  AND year <= '2018'
-	ORDER BY year;
+	FROM economies e 
+	WHERE e.country LIKE '%euro%'
+	)
+		SELECT 
+		* 	
+		FROM Europe
+		WHERE year >= '2006'  AND year <= '2018'
+		ORDER BY year;
 
 
 
