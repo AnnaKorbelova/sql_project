f2fec3cf7ded4b8dd3ffcec6895901326a5c3a6d
Anna Korbelová <137218395+AnnaKorbelova@users.noreply.github.com>
2023-09-28 20:08:15 +0200

    Update answer_3.sql fix
    vložení pohledu a oprava názvů

--- answer_3.sql
+++ answer_3.sql
@@ -1,15 +1,27 @@
+CREATE OR REPLACE VIEW v_answer_3V AS
+SELECT DISTINCT 
+		average_wages,
+		food_category,
+		food_average_price,
+		payroll_year
+FROM v_anna_korbelova_project_sql_primary_final vakpspf 
+GROUP BY 	payroll_year,
+			average_wages,
+			food_category,
+			food_average_price;
+
 CREATE OR REPLACE VIEW v_anna_korbelova_sql_answer_3r AS
 SELECT 
-	vo.food_category,  
-	vo.payroll_year,
-	vo2.payroll_year + 1 AS year_prev,
-	ROUND((vo.food_average_price - vo2.food_average_price) / vo2.food_average_price * 100, 2) AS food_average_price_growth
-FROM v_otazka_3 vo
-JOIN v_otazka_3 vo2  
-	ON vo.food_category = vo2.food_category  
-	AND vo.payroll_year = vo2.payroll_year + 1
-GROUP BY vo.food_category,  
-	 vo.payroll_year
+	va.food_category,  
+	va.payroll_year,
+	va2.payroll_year + 1 AS year_prev,
+	ROUND((va.food_average_price - va2.food_average_price) / va2.food_average_price * 100, 2) AS food_average_price_growth
+FROM v_answer_3V va2
+JOIN v_answer_3V va2  
+	ON va.food_category = va2.food_category  
+	AND va.payroll_year = va2.payroll_year + 1
+GROUP BY va.food_category,  
+	 va.payroll_year
 ORDER BY food_average_price_growth;
 
 
@@ -17,10 +29,23 @@ SELECT
 	food_category, 
 	food_average_price_growth,
 CASE 
-	WHEN food_average_price_growth < 1 THEN 'price_slow_growth'
+	WHEN food_average_price_growth < 10 THEN 'price_slow_growth'
 	WHEN food_average_price_growth < 40 THEN 'price_fast_growth'
 	ELSE 'price_hight_growth'
 	END AS price_growth
 FROM v_anna_korbelova_sql_answer_3r 
-GROUP BY food_category  
+GROUP BY food_category;
+
+SELECT 
+		food_category, 
+		food_average_price_growth,
+	CASE 
+		WHEN food_average_price_growth < 10 THEN 'price_slow_growth'
+		WHEN food_average_price_growth < 40 THEN 'price_fast_growth'
+		ELSE 'price_hight_growth'
+		END AS price_growth
+FROM v_anna_korbelova_sql_answer_3r 
+GROUP BY food_category,
+	 food_average_price_growth
+ORDER BY food_average_price_growth
 LIMIT 1;
