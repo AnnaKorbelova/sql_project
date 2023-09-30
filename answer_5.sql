ec2a04d7cf1e121bf32a7b4ff1b2669438dc1f29
Anna Korbelová <137218395+AnnaKorbelova@users.noreply.github.com>
2023-09-30 15:49:23 +0200

    New answer_5
    Otázka, vypracována znovu, pomocí nových pohledů.

---
+++ answer_5.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE VIEW v_czech_rep AS
+SELECT 
+		e.country,
+		e.GDP,
+		e.population,
+		e.year
+FROM economies e 
+	JOIN countries c 
+	ON e.country = c.country
+WHERE e.year >= '2006'  AND e.year <= '2018' AND e.country = 'Czech Republic';
+
+
+CREATE OR REPLACE VIEW v_anna_korbelova_sql_answer_5 AS
+SELECT 
+		va4v.payroll_year,
+		va4v.year_prev,
+		va4v.food_average_price_growth,
+		va4v.average_wages_growth,
+		vcr.GDP  
+FROM v_czech_rep vcr
+	JOIN v_answer_4v va4v 
+	ON va4v.payroll_year = vcr.YEAR
+GROUP BY va4v.payroll_year,
+		 va4v.year_prev,
+		 va4v.food_average_price_growth,
+		 va4v.average_wages_growth,
+		 vcr.GDP;
+	
+
